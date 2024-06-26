2348894b72e17450533f866e18d9531a9d9669fb
Justin Bertrand <justin.bertrand@ssc-spc.gc.ca>
2024-06-26 12:23:03 -0400

    chore: move configs to its own repo.

--- data.tf
+++ data.tf
@@ -1,12 +1,1 @@
 data "azurerm_client_config" "current" {}
-
-data "azuread_users" "canchat_admins" {
-  user_principal_names = [
-    "justin.bertrand@ssc-spc.gc.ca",
-    "pascal.beaulne2@ssc-spc.gc.ca",
-  ]
-}
-
-data "azurerm_dns_zone" "dsai_dns" {
-  name = "dsai-sdia.ssc-spc.cloud-nuage.canada.ca"
-}
